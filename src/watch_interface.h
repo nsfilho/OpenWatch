b29d7ae01ff063c8e6a3586d3bb8df8a8226c27b
Nelio Santos <nelio.santos@jbtec.com.br>
2020-02-23 12:24:11 -0300

    feat: adding GLPv3 license

--- src/watch_interface.h
+++ src/watch_interface.h
@@ -1,3 +1,21 @@
+/**
+ * Openwatch Framework
+ * Copyright (C) 2020  Nelio Souza Santos Filho <nsfilho@icloud.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+ *
+ */
 #ifndef __WATCH_INTERFACE_H_
 #define __WATCH_INTERFACE_H_
 
@@ -9,16 +27,33 @@
 #define BATTERY_BAR_WIDTH(X) (((M5.Axp.GetBatVoltage() - MIN_VOLTAGE) * X) / (MAX_VOLTAGE - MIN_VOLTAGE))
 #define BATTERY_LVL ((M5.Axp.GetBatVoltage() - MIN_VOLTAGE) / (MAX_VOLTAGE - MIN_VOLTAGE)) * 100
 
-#define POSITION_BAR_TOP 0
-#define POSITION_BAR_RIGHT 1
-#define POSITION_BAR_BOTTOM 2
-#define POSITION_BAR_LEFT 3
-#define POSITION_BAR_OFF 4
-
-#define NETWORK_STATUS_NONE 0
-#define NETWORK_STATUS_SMALL 1
-#define NETWORK_STATUS_DIALOG 2
 #define NETWORK_STATUS_DIALOG_TIMEOUT 3000
+
+typedef enum
+{
+    POSITION_BAR_TOP = 0,
+    POSITION_BAR_RIGHT = 1,
+    POSITION_BAR_BOTTOM = 2,
+    POSITION_BAR_LEFT = 3,
+    POSITION_BAR_OFF = 4
+} display_position_t;
+
+typedef enum
+{
+    NETWORK_STATUS_NONE = 0,
+    NETWORK_STATUS_DIALOG = 1,
+    NETWORK_STATUS_SMALL_TOP_RIGHT = 2,
+    NETWORK_STATUS_SMALL_TOP_LEFT = 3,
+    NETWORK_STATUS_SMALL_BOTTOM_RIGHT = 4,
+    NETWORK_STATUS_SMALL_BOTTOM_LEFT = 5,
+} display_network_status_t;
+
+typedef enum
+{
+    PERIOD_MORNING = 0,
+    PERIOD_AFTERNOON = 1,
+    PERIOD_EVENING = 2,
+} day_period_t;
 
 class WatchInterface
 {
@@ -32,9 +67,9 @@ private:
     bool loopNetwork();
 
 protected:
-    byte batteryPos = POSITION_BAR_OFF;
-    byte screenPos = POSITION_BAR_OFF;
-    byte networkStatus = NETWORK_STATUS_DIALOG;
+    display_position_t batteryPos = POSITION_BAR_OFF;
+    display_position_t screenPos = POSITION_BAR_OFF;
+    display_network_status_t networkStatus = NETWORK_STATUS_DIALOG;
     virtual bool loop();
 
 public:
